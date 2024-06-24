9d7a37656c7dca0675319249d5b4391443909ea8
Dissembler123 <zzh200926@gmail.com>
2024-06-24 14:09:41 -0400

    documentaion

--- src/entities/Character.java
+++ src/entities/Character.java
@@ -1,6 +1,11 @@
 package entities;
 
 public abstract class Character {
+    """ A Character with string name and int health is abstract and
+    can be extended to child classes like Player(controllable),
+    NPC(uncontrollable). If a character's health <= 0, then the character
+    can't take any action.
+    """
     private String name;
     private int health;
 
@@ -11,7 +16,7 @@ public abstract class Character {
 
     public String getName() {
         return name;
-    }
+    })
 
     public int getHealth() {
         return health;
