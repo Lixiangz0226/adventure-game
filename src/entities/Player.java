2ac96bcc6350fe7b3b6b29b16681da876c74a4cf
Dissembler123 <zzh200926@gmail.com>
2024-06-24 14:41:04 -0400

    documentation and states

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player extends Character {
+    """ A Player is the only controllable character for the user.
+    A Player has a string name, an int health, a list of State state_set,
+    an Inventory inventory and a list of Skill skill_set.
+    """
     private Inventory inventory;
     private List<Skill> skills;
+    private List<State> states;
     private int money;
 
     public Player(String name, int health) {
@@ -13,6 +18,7 @@ public class Player extends Character {
         this.inventory = new Inventory();
         this.skills = new ArrayList<>();
         this.money = 0;
+        this.states = new ArrayList<>();
     }
 
     public Inventory getInventory() {
