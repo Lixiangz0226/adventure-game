7815e171aa3c0f180e36b15676a5dd7160153e5d
Dissembler123 <zzh200926@gmail.com>
2024-06-24 20:42:55 -0400

    Game class

--- src/entities/Game.java
+++ src/entities/Game.java
@@ -1,1 +1,25 @@
+import java.awt.Color;
+import java.awt.Container;
+
+import java.swing.JFrame;
+
+public class Game{
+    JFrame window;
+    Container con;
+
+    public static void main(String[] args){
+        new Game();
+    }
+
+    public Game(){
+
+        window = new JFrame();
+        window.setsize(1200,  900);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+        window.setVisible(true);
+        con = window.getContentPane();
+    }
+}
 
