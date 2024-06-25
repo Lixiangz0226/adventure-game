585056013d72fe5559722a9af39d4893834ccb36
Dissembler123 <zzh200926@gmail.com>
2024-06-25 13:07:14 -0400

    documentation

--- src/entities/Skill.java
+++ src/entities/Skill.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Skill {
+    """Skills are special attacks that has different effacts
+    an can be used by players and bosses. Each has a string
+    <name>, int <damage> and List of State <effact>.
+    """
     private String name;
     private int damage;
     private List<State> effect;
