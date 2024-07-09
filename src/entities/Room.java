7d190cfb57de6a2239859c9ebe4e591e0daa882b
JoJang27 <joseph.octillion@gmail.com>
2024-07-09 13:01:40 -0400

    room changes

--- src/entities/Room.java
+++ src/entities/Room.java
@@ -1,7 +1,6 @@
 package entities;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
@@ -21,28 +20,57 @@ public class Room {
     private List<Interactable_object> interactable_objects;
     private Event events;
     private String name;
-    private Room up;
-    private Room down;
-    private Room left;
-    private Room right;
+    private String description;
+    private int n;
+    private int s;
+    private int w;
+    private int e;
 
-    public Room(String name, Room up, Room down, Room left, Room right, List<Item> items, List<Character> characters,
-                List<Interactable_object> interactable_objects,
-                JTextArea mainTextArea,
-                JButton choice1, JButton choice2, JButton choice3, JButton choice4) {
-
+    public Room(String name, String description, int n, int s, int w, int e) {
+/**
         this.name = name;
         this.characters = characters;
         this.interactable_objects = interactable_objects;
         this.items = items;
         this.mainTextArea = mainTextArea;
         this.choice1 = choice1; this.choice2 = choice2; this.choice3 = choice3; this.choice4 = choice4;
-        this.up = up;
-        this.down = down;
-        this.left = left;
-        this.right = right;
+ */
+        this.n = n;
+        this.s = s;
+        this.w = w;
+        this.e = e;
+    }
+    public int getN() {
+        return n;
+    }
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public int getS() {
+        return s;
+    }
+    public void setS(int s) {
+        this.s = s;
+    }
+
+    public int getW() {
+        return w;
+    }
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getE() {
+        return e;
+    }
+    public void setE(int e) {
+        this.e = e;
     }
 }
+
+
+
 
 
 
