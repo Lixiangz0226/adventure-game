7c3c80cab5e7c9a5664fb06639bca3ab8d999c6f
Robin-Guragain <robinguragain@gmail.com>
2024-07-11 15:59:03 -0400

    Update Map.java

--- src/entities/Map.java
+++ src/entities/Map.java
@@ -3,17 +3,37 @@ package entities;
 import java.util.*;
 
 class Map{
-    /**
-     * A Map is a 2-dimensional plane that contains a List
-     *     of Lists of <rooms> and indicates either daytime or
-     *     nightime <day>.
+   /**
+     * A Map is a hashmap of rooms as nodes of a graph and the values will
+     * be the room to go to from one room. The graph will
+     * be bidirectional. The map indicates either daytime or
+     *     nighttime <day>.
      */
 
-    private List<List<Room>> rooms;
-    private boolean day;
+    public static HashMap<Room, HashMap<String, Room>> world_map = new HashMap<>();
+    private  boolean day;
+    private Room startRoom;
+    private Room playerRoom;
 
-    public Map(List<List<Room>> rooms){
-        this.day = true;
-        this.rooms = rooms;
+    public void addRoom(Room room) {
+        HashMap<String, Room> adjacent_rooms = new HashMap<>();
+        adjacent_rooms.put("Up", room.up);
+        adjacent_rooms.put("Down", room.down);
+        adjacent_rooms.put("Right",room.right);
+        adjacent_rooms.put("Left",room.left);
+        Map.world_map.put(room, adjacent_rooms);
+    }
+
+    public void getDay () {
+
+    }
+
+    public void changeDay () {
+
+    }
+
+    public void displayMap () {
+
+    }
     }
 }
