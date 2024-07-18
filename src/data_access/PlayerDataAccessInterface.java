ace41444975ffc541ccf24c4cb9c27b742c44710
alexandma <maddyalexander123@gmail.com>
2024-07-18 15:51:12 -0400

    create save and load files and add c.a. directories

---
+++ src/data_access/PlayerDataAccessInterface.java
@@ -0,0 +1,11 @@
+package data_access;
+
+// import entities.Player;
+
+public interface PlayerDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    // void save(Player player);
+
+    // Player load();
+}
