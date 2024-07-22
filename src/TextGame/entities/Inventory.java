c14a459d01a09d91741113009033ed67509bcdcb
alexandma <maddyalexander123@gmail.com>
2024-07-21 22:16:32 -0400

    Update Inventory.java

--- src/TextGame/entities/Inventory.java
+++ src/TextGame/entities/Inventory.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Inventory {
     /**
-    Inventories store items in a list of Item items.
-    */
+     * Inventories store items in a list of Item objects.
+     */
 
     private List<Item> items;
 
@@ -14,14 +14,27 @@ public class Inventory {
         this.items = new ArrayList<>();
     }
 
-    public List<Item> getItems() {return items;}
-
-    public Item getItem(int id) {return items.get(id);}
-
-    public void addItem(Item item) {this.items.add(item);}
-
-    public void removeItem(Item o) {this.items.remove(o);}
-
-    public int get_length(){return this.items.size();}
+    public List<Item> getItems() {
+        return new ArrayList<>(items); // Return a copy to protect encapsulation
     }
 
+    public Item getItem(int id) {
+        if (id >= 0 && id < items.size()) {
+            return items.get(id);
+        } else {
+            throw new IndexOutOfBoundsException("Invalid item ID");
+        }
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
+    public int getLength() {
+        return this.items.size();
+    }
+}
