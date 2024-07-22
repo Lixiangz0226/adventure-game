cbcca03460ff08ab986e8ba503677cf3110ec3d0
JoJang27 <joseph.octillion@gmail.com>
2024-07-21 20:41:00 -0400

    Update Item.java
    Added documentation

--- src/TextGame/entities/Item.java
+++ src/TextGame/entities/Item.java
@@ -1,21 +1,22 @@
 package TextGame.entities;
 
+//Items that players can use in puzzles or battles
 public class Item {
 
+    //Item attributes
     public int health;
-    /**
-     * An Item with a unique use.
-     */
     private String name;
 
+    //Item constructor
     public Item (String name) {
         this.name = name;
     }
 
+    //Return the item name
     public String get_name(){return this.name;}
-
 }
 
+//Obtainable item classes in the game
 class Life_Potion extends Item { public Life_Potion() {super("Life Potion");}}
 
 class Golden_Key extends Item{ public Golden_Key(){super("Golden key");}}
