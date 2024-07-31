b55de098a2e0a3fbe384152921f17ee18dfa23e9
Octillion27 <joseph.jang@mail.utoronto.ca>
2024-07-31 08:03:47 -0400

    Added accuracy, critical rate, and weapon types to the weapon class

--- src/TextGame/entities/Weapons/Weapon.java
+++ src/TextGame/entities/Weapons/Weapon.java
@@ -5,16 +5,35 @@ import TextGame.entities.Items.Item;
 public class Weapon extends Item {
     /**
      * Represents a weapon that can be used by characters in the game.
-     * A weapon has a name and a certain amount of damage it can inflict.
+     * A weapon has a name, damage, accuracy, and a critical rate.
+     * Weapons can deal physical or magical damage
+     * Some Weapons are effective against flying enemies
      */
-    private int damage;
 
-    public Weapon(String name, int damage){
+    private int damage;
+    private float accuracy;
+    private float critical_rate;
+    private boolean physical_damage;
+    private boolean flying_bonus;
+
+    public Weapon(String name, int damage, float accuracy){
         super(name);
         this.damage = damage;
+        this.accuracy = accuracy;
+        this.critical_rate = critical_rate;
+        this.physical_damage = true;
+        this.flying_bonus = false;
     }
 
     public int get_damage(){return this.damage;}
+
+    public float get_accuracy(){return this.accuracy;}
+
+    public float get_critical_rate(){return this.critical_rate;}
+
+    public boolean check_physical_damage(){return this.physical_damage;}
+
+    public boolean check_flying_bonus(){return this.flying_bonus;}
 
 }
 
