de23ba495e12f1bd7fe66ca323ad616a55609c81
Zihan Zhang <zzh200926@gmail.com>
2024-08-12 23:51:01 -0400

    data_access

---
+++ src/data_access/SaveEvent.java
@@ -0,0 +1,24 @@
+package data_access;
+
+import controller.GamePanel;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveEvent {
+    File file = new File("src\\data_access\\Player.txt");
+    GamePanel gp;
+
+    public SaveEvent(GamePanel gp) {this.gp = gp;}
+
+    public void save() throws IOException {
+        file.delete();
+        file.createNewFile();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        // Cursed Flower()
+        writer.write(gp.cursedFlower.position);
+    }
+}
